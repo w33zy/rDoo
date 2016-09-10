077247867d415bb859253d8f1d7fd16c3e681985
w33zy <wzy@outlook.com>
2016-09-10 13:06:58 -0500

    Applied new design to old task logic

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   
   get 'about' => 'pages#about'
 
+  get 'test' => 'pages#test'
+
   root 'pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
