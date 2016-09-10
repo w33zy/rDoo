adbb2f030a102980341e28f30f13389a60e1c99d
w33zy <wzy@outlook.com>
2016-09-09 22:31:45 -0500

    Added change action and custom route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :tasks
+  resources :tasks do
+    member do
+      put :change
+    end
+  end
+
   devise_for :users
+
   get 'pages/home'
   
   get 'about' => 'pages#about'
